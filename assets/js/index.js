24a1b1ece2fd081b709ce4d2d4517f4db3e499fa
Lupyana Mbembati <lupianovic21@gmail.com>
2017-08-25 16:40:53 +0300

    added new nav and team

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,31 +9,31 @@ jQuery(window).load(function(){
 });
 
 $(document).ready(function(){
+	//
+	// $(window).scroll(function () {
+	// 		if ($(window).scrollTop() > 400) {
+	// 				$(".navbar-brand a").css("color","#fff");
+	// 				$("#navigation").removeClass("animated-header");
+	// 		} else {
+	// 				$(".navbar-brand a").css("color","inherit");
+	// 				$("#navigation").addClass("animated-header");
+	// 		}
+	// });
 
-	$(window).scroll(function () {
-			if ($(window).scrollTop() > 400) {
-					$(".navbar-brand a").css("color","#fff");
-					$("#navigation").removeClass("animated-header");
-			} else {
-					$(".navbar-brand a").css("color","inherit");
-					$("#navigation").addClass("animated-header");
-			}
-	});
-
-	$('#nav').singlePageNav({
-		offset: jQuery('#nav').outerHeight(),
-		filter: ':not(.external)',
-		speed: 2000,
-		currentClass: 'current',
-		easing: 'easeInOutExpo',
-		updateHash: true,
-		beforeStart: function() {
-			console.log('begin scrolling');
-		},
-		onComplete: function() {
-			console.log('done scrolling');
-		}
-	});
+	// $('#nav').singlePageNav({
+	// 	offset: jQuery('#nav').outerHeight(),
+	// 	filter: ':not(.external)',
+	// 	speed: 2000,
+	// 	currentClass: 'current',
+	// 	easing: 'easeInOutExpo',
+	// 	updateHash: true,
+	// 	beforeStart: function() {
+	// 		console.log('begin scrolling');
+	// 	},
+	// 	onComplete: function() {
+	// 		console.log('done scrolling');
+	// 	}
+	// });
 
   $(".owl-carousel").owlCarousel({
 		 loop:true,
@@ -59,6 +59,100 @@ $(document).ready(function(){
 
 	}
 })
+
+
+	var secondaryNav = $('.cd-secondary-nav'),
+		secondaryNavTopPosition = secondaryNav.offset().top,
+		taglineOffesetTop = $('#cd-intro-tagline').offset().top + $('#cd-intro-tagline').height() + parseInt($('#cd-intro-tagline').css('paddingTop').replace('px', '')),
+		contentSections = $('.cd-section');
+
+		console.log(taglineOffesetTop);
+		console.log(secondaryNavTopPosition);
+	$(window).on('scroll', function(){
+		//on desktop - assign a position fixed to logo and action button and move them outside the viewport
+		( $(window).scrollTop() > taglineOffesetTop ) ? $('#cd-logo, .cd-btn').addClass('is-hidden') : $('#cd-logo, .cd-btn').removeClass('is-hidden');
+		console.log();
+		//on desktop - fix secondary navigation on scrolling
+		if($(window).scrollTop() > secondaryNavTopPosition ) {
+			//fix secondary navigation
+			secondaryNav.addClass('is-fixed');
+			//push the .cd-main-content giving it a top-margin
+			$('.cd-main-content').addClass('has-top-margin');
+			//on Firefox CSS transition/animation fails when parent element changes position attribute
+			//so we to change secondary navigation childrens attributes after having changed its position value
+			setTimeout(function() {
+	            secondaryNav.addClass('animate-children');
+	            $('#cd-logo').addClass('slide-in');
+				$('.cd-btn').addClass('slide-in');
+	        }, 50);
+		} else {
+			secondaryNav.removeClass('is-fixed');
+			$('.cd-main-content').removeClass('has-top-margin');
+			setTimeout(function() {
+	            secondaryNav.removeClass('animate-children');
+	            $('#cd-logo').removeClass('slide-in');
+				$('.cd-btn').removeClass('slide-in');
+	        }, 50);
+		}
+
+		//on desktop - update the active link in the secondary fixed navigation
+		// updateSecondaryNavigation();/
+	});
+
+	function updateSecondaryNavigation() {
+
+		// console.log(contentSections);
+		contentSections.each(function(){
+			var actual = $(this),
+				actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
+				actualAnchor = secondaryNav.find('a[href="#'+actual.attr('id')+'"]');
+			if ( ( actual.offset().top - secondaryNav.height() <= $(window).scrollTop() ) && ( actual.offset().top +  actualHeight - secondaryNav.height() > $(window).scrollTop() ) ) {
+				actualAnchor.addClass('active');
+			}else {
+				actualAnchor.removeClass('active');
+			}
+		});
+
+	}
+
+	//on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
+	$('.cd-secondary-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		$(this).toggleClass('menu-is-open');
+		secondaryNav.find('ul').toggleClass('is-visible');
+	});
+
+	// //smooth scrolling when clicking on the secondary navigation items
+	// secondaryNav.find('ul a').on('click', function(event){
+  //       event.preventDefault();
+  //       var target= $(this.hash);
+  //       $('body,html').animate({
+  //       	'scrollTop': target.offset().top - secondaryNav.height() + 1
+  //       	}, 400
+  //       );
+  //       //on mobile - close secondary navigation
+  //       $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
+  //       secondaryNav.find('ul').removeClass('is-visible');
+  //   });
+
+	secondaryNav.singlePageNav({
+		offset: jQuery('#nav').outerHeight(),
+		filter: ':not(.external)',
+		speed: 2000,
+		currentClass: 'current',
+		easing: 'easeInOutExpo',
+		updateHash: true,
+		beforeStart: function() {
+			console.log('begin scrolling');
+		},
+		onComplete: function() {
+			console.log('done scrolling');
+		}
+	});
+    //on mobile - open/close primary navigation clicking/tapping the menu icon
+	$('.cd-primary-nav').on('click', function(event){
+		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+	});
 
 });
 
