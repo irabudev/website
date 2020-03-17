075d3e6104ff4cf6c7151573e5bdeeca32dc3065
Lupyana <lupyanambembati@gmail.com>
2020-03-17 13:03:51 +0300

    Feat: Maintenance page
    - Added the maintenance page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,36 +65,21 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
+                <lottie-player
+                    src="https://assets8.lottiefiles.com/packages/lf20_xRmNN8.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay >
+                </lottie-player>
+
                 <div class="title m-b-md">
-                    Laravel
+                    Be right back
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <p>We are working on a better experience</p>
                 </div>
             </div>
         </div>
     </body>
 </html>
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
