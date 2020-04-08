f0a9907ac85829864c935d4ebcf07f88caa58052
Lupyana Mbembati <lupyanambembati@gmail.com>
2020-04-08 23:37:38 +0300

    refactor: moved the different client style to own sections

---
+++ resources/views/partials/clients/clients.blade.php
@@ -0,0 +1,34 @@
+<div class="rev_redraw-wrapper ">
+    <div class="rev_redraw-inner-box  bg-gray-2  section-space--mt_40 section-space--mb_40  rev_redraw-space">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="redraw-content text-center">
+                        <h3 class="font-weight--reguler">Honarable mentions <span class="text-color-primary">at its best</span></h3>
+                        <p class="redwaw-dec">How IT services change people's life for the better </p>
+                        <div class="bedraw-button">
+                            <a href="index-cybersecurity.html#" class="ht-btn ht-btn-md">Download free proposal sample <i class="far fa-long-arrow-right ml-2"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="redraw-images">
+                        <img class="img-fluid redrow-1 animate wow fadeInLeft  width-160" data-wow-delay="0.1s" src="{{ asset('/images/logo.svg') }}" alt="">
+
+                        <img class="img-fluid redrow-2 animate wow fadeInLeft  width-160" data-wow-delay="0.2s" src="{{ asset('/images/logo.svg') }}" alt="">
+
+                        <img class="img-fluid redrow-3 animate wow fadeInLeft  width-160" data-wow-delay="0.3s" src="{{ asset('/images/logo.svg') }}" alt="">
+
+                        <img class="img-fluid redrow-4 animate wow fadeInLeft  width-160" data-wow-delay="0.4s" src="{{ asset('/images/logo.svg') }}" alt="">
+
+                        <img class="img-fluid redrow-5  animate wow fadeInLeft  width-160" data-wow-delay="0.1s" src="{{ asset('/images/logo.svg') }}" alt="">
+
+                        <img class="img-fluid redrow-6 animate wow fadeInLeft  width-160" data-wow-delay="0.3s" src="{{ asset('/images/logo.svg') }}" alt="">
+
+                        <img class="img-fluid redrow-7 animate wow fadeInLeft  width-160" data-wow-delay="0.4s" src="{{ asset('/images/logo.svg') }}" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/partials/clients/clients_alt.blade.php
@@ -0,0 +1,122 @@
+<div class="tabs-wrapper bg-gray section-space--ptb_100">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-title-wrapper text-center section-space--mb_60 wow move-up">
+                    <h6 class="section-sub-title mb-20">discover our company</h6>
+                    <h3 class="section-title">We’ve been thriving in<span class="text-color-primary"> 38 years</span> </h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 ht-tab-wrap">
+
+
+                <div class="tab-content ht-tab__content">
+
+                    <div class="tab-pane  fade show active" id="tab_list_08" role="tabpanel">
+                        <div class="tab-history-wrap section-space--mt_60 brand-logo-slider__one">
+                            <div class="modern-grid--6">
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-01-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-02-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-03-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-04-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-05-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-06-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-07-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-08-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="brand-logo brand-logo--slider">
+                                    <a href="index-appointment.html#">
+                                        <div class="brand-logo__image">
+                                            <img src="{{ asset('/images/logo.svg') }}" class="img-fluid width-160" alt="">
+                                        </div>
+                                        <div class="brand-logo__image-hover">
+                                            <img src="assets/images/brand/mitech-client-logo-09-hover.png" class="img-fluid" alt="">
+                                        </div>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
