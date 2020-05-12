0dfa0bebcee1b1b5c79c2ff21c9d923cd47ee954
mtuchi <mtuchidev@gmail.com>
2020-05-12 13:20:55 +0300

    feat: add rpi and kuli asset

--- resources/views/pages/home/hero.blade.php
+++ resources/views/pages/home/hero.blade.php
@@ -1,26 +1,27 @@
 <div class="preview-hero-area preview-hero-bg position-relative section-space--ptb_120">
-    <div class="container-fluid container-fluid--cp-150">
+    <div class="container container-fluid--cp-">
         <div class="row align-items-center">
             <div class="col-lg-6">
                 <div class="preview-hero-text wow move-up">
-                    <h1 class="font-weight--reguler mb-30 intro-slogan">Building,to simplify</h1>
-                    <h6 class="mb-30">We craft SIMPLE & VALUABLE
-                        solutions to everday challenges</h6>
-                    <!-- <div class="hero-button mt-40">
-                        <a href="index.html#section-demos" class="btn btn--white">Browse demos </a>
-                    </div> -->
+                    <h1 class="font-weight--reguler mb-30 intro-slogan">{{__('Build to simplify')}}</h1>
+                    <h6 class="mb-30 text-black">
+                        {{__('We craft SIMPLE & VALUABLE solutions to everday challenges')}}
+                    </h6>
+                    <div class="hero-button mt-40">
+                        <a href="#section-demos" class="btn btn--primary">{{__('See what we do')}} </a>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="preview-inner-img">
                     <img class="img-fluid min-img  animation_images one wow fadeInDown"
-                        src="{{ asset('images/hero/mitech-landing-main-slider-slide-01-image-04.png') }}" alt="">
+                        src="{{ asset('images/hero/kuli.png') }}" alt="Kuli">
                 </div>
             </div>
         </div>
 
-        <img class="img-fluid pr-img-01 animation_images two wow fadeInDown"
-            src="{{ asset('images/hero/mitech-landing-main-slider-slide-01-image-01.png') }}" alt="">
+        <img class="img-fluid pr-img-01 animation_images two wow fadeInDown" src="{{ asset('images/hero/rpi.png') }}"
+            alt="IoT">
         <img class="img-fluid pr-img-02 animation_images two wow fadeInDown"
             src="{{ asset('images/hero/mitech-landing-main-slider-slide-01-image-02.png') }}" alt="">
 
