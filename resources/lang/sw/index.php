e01f1c405ec2ccc2e42c71122f13c46d3296d5a8
Stewart Nyiti <stewartnyiti@gmail.com>
2020-05-23 05:08:50 +0300

    Update index.php

--- resources/lang/sw/index.php
+++ resources/lang/sw/index.php
@@ -7,7 +7,7 @@ return [
     | Home page Kiswahili  translations
     |-------------------------------------
     */
-    'gorocket' => 'Rahisi, iliyojaa na yenye kubinafsishika ni kitendea kazi kilichoegemea mtandaoni ambacho kinakusaidia kusimamia dili zako, mauzo na hatua za kukabidhi bidhaa; hatima yake hunyanyua mauzo na uzalishaji! ',
-    '78inc' => 'Ilianzishwa mnamo 2019, 78inc ni shirika la ubunifu ambalo hutumia usanifu, uzoefu, utu na ujuzi wa soko ili kutoa huduma za kidigitali kwa werevu. Licha ya kuwa biashara mpya 78inc wamekuwa na bahati ya kutosha kuvuna imani kwa baadhi ya biashara za nyumbani Tanzania na nje ya nchi. '
+    'gorocket' => 'Rahisi, kilichojaa na chenye kubinafsishika. Ni kitendea kazi kilichoegemea mtandaoni ambacho kinakusaidia kusimamia dili zako, mauzo na hatua za kukabidhi bidhaa; hatima yake ni kunyanyua mauzo yako na uzalishaji! ',
+    '78inc' => 'Ilianzishwa mnamo 2019, 78inc ni shirika la ubunifu ambalo hutumia usanifu, uzoefu, utu na ujuzi wa soko ili kutoa huduma za kidigitali kwa werevu na weledi. Licha ya kuwa biashara mpya 78inc imekuwa na bahati ya kutosha kuvuna imani kwa baadhi ya biashara za nyumbani Tanzania na hata nje ya nchi. '
 
 ];
