ee9392a0ae0e669ca2f89c11e72a09ddf1cf64f5
Stewart Nyiti <stewartnyiti@gmail.com>
2020-07-17 07:21:55 +0300

    Update process.php

--- resources/lang/sw/process.php
+++ resources/lang/sw/process.php
@@ -8,11 +8,11 @@ return [
     |-------------------------------------
     */
 
-    "discovery" => "Tunajifunza zaidi juu ya biashara ya mteja na kuchunguza mbinu zao za sasa za kutatua tatizo ili kupata ufahamu ambao utatuarifu na kutuongoza wakati wote wa mradi",
+    "discovery" => "Tunaanza kwa kujifunza zaidi juu ya biashara ya mteja kwa kuchunguza mbinu zake za sasa za kutatua tatizo, ili kupata ufahamu ambao utatuarifu na kutuongoza wakati wote wa mradi",
     "define" => "Hatua hii kwa ujumla inahusu ukuzaji wa fikra kwa kulenga kwenye lengo kuu la kipande na kuzuia kuning'inia kwenye maelezo madogo madogo",
     "design" => "Sasa tunaweza kuanza kuendeleza vyote hivi kwa usanifu wa hali ya juu na kuanza kurudiarudia kwenye mawazo yetu",
     "develop" => "Tunaendelea kusafisha mitindo, hadithi na rasilimali. Kufikia mwisho wa hatua hii tayari tutakuwa tumemaliza 95.998328% ya mradi wetu",
-    "concepts" => "Wataalam wetu wanakuja na kila aina ya fikra na njia za kutoa suluhisho bora kwa huduma za TEHAMA zilizochaguliwa.",
+    "concepts" => "Hapa wataalam wetu wanakuja na kila aina ya kila aina ya njia na fikra za kutoa suluhisho bora kwa huduma za TEHAMA zilizochaguliwa.",
     "testing" => "Baada ya kukubaliana juu ya maoni na mipango, tutafanya kama ilivyopangwa na tutatoa maoni juu ya matokeo na marekebisho.",
     "execute" => "Mara tu mpango wa mwisho ukishapitishwa, kila kitu kitafanywa kulingana na mkataba uliokubaliwa."
 ];
