bbbf1703ffade9e4a8c25907cbe1ab41f29f7bc6
Stewart Nyiti <stewartnyiti@gmail.com>
2020-07-18 21:46:56 +0300

    Update process.php

--- resources/lang/sw/process.php
+++ resources/lang/sw/process.php
@@ -12,9 +12,8 @@ return [
     "define" => "Hatua hii kwa ujumla inahusu ukuzaji wa fikra kwa kulenga kwenye lengo kuu la kipande na kuzuia kuning'inia kwenye maelezo madogo madogo",
     "design" => "Sasa tunaweza kuanza kuendeleza vyote hivi kwa usanifu wa hali ya juu na kuanza kurudiarudia kwenye mawazo yetu",
     "develop" => "Tunaendelea kusafisha mitindo, hadithi na rasilimali. Kufikia mwisho wa hatua hii tayari tutakuwa tumemaliza 95.998328% ya mradi wetu",
-    "concepts" => "Hapa wataalam wetu wanakuja na kila aina ya kila aina ya njia na fikra za kutoa suluhisho bora kwa huduma za TEHAMA zilizochaguliwa.",
+    "concepts" => "Hapa wataalam wetu wanakuja na kila aina ya njia na fikra za kutoa suluhisho bora kwa huduma za TEHAMA zilizochaguliwa.",
     "testing" => "Baada ya kukubaliana juu ya maoni na mipango, tutafanya kama ilivyopangwa na tutatoa maoni juu ya matokeo na marekebisho.",
     "execute" => "Mara tu mpango wa mwisho ukishapitishwa, kila kitu kitafanywa kulingana na mkataba uliokubaliwa.",
-    "description" => "SW: About our preferred tools to build modern web applications. Read more on our technology stack and hire
-    us as a team",
+    "description" => "Kuhusu zana zetu tunazopendelea kuunda programu za kisasa za mtandao. Soma zaidi kwenye rundo letu la teknolojia na uajiri timu yetu",
 ];
